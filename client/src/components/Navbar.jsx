e0882f2850c1a18bff26605b938000df5183c823
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-22 14:59:27 +0100

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { HiMenuAlt4 } from 'react-icons/h';
+import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons-ai';
 import logo from '../../images/logo.png';
 
