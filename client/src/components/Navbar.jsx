205cb5e942fa2645740b328442a25d06d678b1f5
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-22 14:48:39 +0100

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,10 @@
 import { HiMenuAlt4 } from 'react-icons/h';
 import { AiOutlineClose } from 'react-icons-ai';
-
+import logo from '../../images/logo.png';
 
 const Navbar = () => {
     return (
-
         <h1>Navbar</h1>
-    
     )
 }
 export default Navbar
