ff011c11550bd97162b1ce33a9c9614754c80c68
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-22 21:40:02 +0100

    changes

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,19 @@
-import { AiFillPlayCircle } from "react-icons/ai"
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
-        
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className=" flex flex-1 justify-start flex-col md:mr-10">
+                    <h1>
+                        Send Crypto <br/> across the world
+                    </h1>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Welcome
