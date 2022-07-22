116ee3076c98dff385b361d8982de0825ea711a3
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-22 20:41:32 +0100

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,14 +9,16 @@ const NavbarItem = ({title, classProps}) => {
     )
 }
 
-const Navbar = () => {
+const Navbar = () => { 
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className="md:flex-0.5 flex-initial justify-center items-center">
                 <img src={logo} alt="log" className='w-32 cursor-pointer' />
             </div>
-            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-
+            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                    <NavbarItem key={item + index} title={item}/>
+                ))}
             </ul>
         </nav> 
     )
