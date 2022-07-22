f3c1748d47cd0451b1543cd4d6b2be3bcb15cf6e
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-22 17:34:34 +0100

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,11 @@
 import logo from '../../images/logo.png';
 
 const NavbarItem = ({title, classProps}) => {
-
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
 }
 
 const Navbar = () => {
