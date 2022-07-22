49505d5e381f863779bc4a5663d4f292e42f8827
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-22 13:04:27 +0100

    changes

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+    <div className="App">
+       <h1 class="text-3xl font-bold underline">
+          Hello world!
+       </h1>
+    </div>
+  )
+}
+
+export default App
