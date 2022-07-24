cce6a11892c756c77d8b33f7ee86808b40228c0f
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-24 17:52:07 +0100

    changes

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,5 @@
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+pragma solidity ^0.8.0;
