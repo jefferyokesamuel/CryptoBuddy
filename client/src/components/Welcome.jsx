0032b04261ab608910e424539bd0c58faabefc7e
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-07-24 16:06:45 +0100

    changes

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,9 +6,14 @@ import { Loader } from "./";
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
-const Input = (placeholder) => (
+const Input = (placeholder, name, type, value, handleChange)  => (
     <input
     placeholder={placeholder}
+    type={type}
+    step="0.001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2, w-full rounded-sm p-2"
     />
 ) 
 const Welcome = () => {
