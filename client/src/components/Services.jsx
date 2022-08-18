8fb6458443950f34103f91db7d3ff6f24e7bd5d7
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-08-18 15:27:26 +0100

    changes

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -7,9 +7,10 @@ const ServiceCard = ({color, title, icon, subtitle}) => (
         <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}> 
             {icon}
         </div>
-        <div>
-            
-        </div>
+        <div className='ml-5 flex flex-col flex-1'>
+            <h1 className='mt-2 text-white text-lg'>{title}</h1>
+            <p className='mt-2 text-white text-sm md:w-9/12'>{subtitle}</p>
+        </div> 
     </div>
 )
 
