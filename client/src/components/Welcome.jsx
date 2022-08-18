89a97fc6003590eef4c71a10a3447672655416bc
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-08-18 11:48:38 +0100

    changes

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -91,10 +91,10 @@ const Welcome = () => {
                         </div>
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder="Address To" name='addressTo' type='text' handleChange={() => {}}/>
-                        <Input placeholder="Amount (ETH)" name='amount' type='number' handleChange={() => {}}/>
-                        <Input placeholder="Keyword (GIF)" name='keyword' type='text' handleChange={() => {}}/>
-                        <Input placeholder="Enter Message" name='message' type='text' handleChange={() => {}}/>
+                        <Input placeholder="Address To" name='addressTo' type='text' handleChange={handleChange}/>
+                        <Input placeholder="Amount (ETH)" name='amount' type='number' handleChange={handleChange}/>
+                        <Input placeholder="Keyword (GIF)" name='keyword' type='text' handleChange={handleChange}/>
+                        <Input placeholder="Enter Message" name='message' type='text' handleChange={handleChange}/>
 
                         <div className="h-[1px] w-full bg-gray-400 m-2"/>
 
