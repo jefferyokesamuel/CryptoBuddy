782b9b2a84bed4b9b1befa7b0d1bb793c5411f53
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-08-18 16:02:57 +0100

    changes

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,8 +1,14 @@
+import React, { useContext } from "react";
+
+import { TransactionContext } from "../context/TransactionContext";
+
+import {dummyData} from "../utils/dummyData";
+
 const Transactions = () => {
     return (
+        <div className="flex-w full justify-center items-center 2xl:px">
 
-        <h1>Transactions</h1>
-        
+        </div>
     )
 }
 export default Transactions
