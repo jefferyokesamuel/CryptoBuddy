98eba6c12c895d83b493824852c3c22091f9ee28
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-08-18 16:30:43 +0100

    changes

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 
 import { TransactionContext } from "../context/TransactionContext";
 
+import { shortenAddress } from "../utils/shortenAddress";
+
 import {dummyData} from "../utils/dummyData";
 
 const Transactions = () => {
@@ -11,8 +13,13 @@ const Transactions = () => {
             <div className="flex flex-col md:p-12 py-12 px-4">
                 {currentAccount ? (
                     <h3 className="text-white text-3xl text-center my-3"> Latest Transactions</h3>
-                )
-                }
+                ):(
+                    <h3 className="text-white text-3xl text-center my-3"> Connect your Account to see latest changes</h3>
+                )}
+
+                <div className="flex flex-wrap justify-center items-center mt-10">
+                    {dummyData.reverse}
+                </div>
             </div>
         </div>
     )
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,6 +5,7 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
+import { shortenAddress } from "../utils/shortenAddress";
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
 
@@ -21,7 +22,7 @@ const Input = ({placeholder, name, type, value, handleChange})  => (
 const Welcome = () => {
 
 
-    const { connectWallet, connectAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext)
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext)
 
     const handleSubmit = (e) => {
         const { addressTo, amount, keyword, message } = formData
@@ -44,7 +45,7 @@ const Welcome = () => {
                         Explore the Crypto world. Buy and sell cryptocurrencies with CryptoBuddy
                     </p>
                     
-                    {!connectAccount && (
+                    {!currentAccount && (
                         <button
                         type="button"
                         onClick={connectWallet}
@@ -87,7 +88,7 @@ const Welcome = () => {
                             </div>
                                 <div>
                                     <p className="text-white font-light text-sm">
-                                        Address
+                                        {shortenAddress(currentAccount)}
                                     </p>
                                     <p className="text-white font-semibold text-lg mt-1">
                                         Ethereum
