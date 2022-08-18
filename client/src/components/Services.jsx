037b4f9feeb80fe966e32930a7281c35871729df
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2022-08-18 15:06:04 +0100

    changes

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,12 +1,11 @@
 import { BsShieldFillCheck } from 'react-icons/bs';
 import { BiSearchAlt } from 'react-icons/bi';
-import { RiHeart2Fill}
+import { RiHeart2Fill } from 'react-icons/ri';
 
 const Services = () => {
     return (
+        <div className="flex w-full justify-center items-center gradient-bg-services"
 
-        <h1>Services</h1>
-        
     )
 }
 export default Services
